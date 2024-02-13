246751141dc3700efcd5a311fe12c9f13b5d0f68
Ankcorn <thomasankcorn@gmail.com>
2024-02-13 15:18:16 +0000

    custom request id header2

--- svelte-opentelemetry-vercel/src/routes/+page.svelte
+++ svelte-opentelemetry-vercel/src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
+	
 </script>
 
 <svelte:head>
